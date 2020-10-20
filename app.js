e9a09d9384986f77e7281497650e5122549fd854
dustinsnoap <dustinsnoap@gmail.com>
2020-10-20 14:37:18 -0400

    set up basics

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {graphqlHTTP} = require('express-graphql') //export valid middleware function
+const {buildSchema} = require('graphql')
 require('./motd/motd')
-const PORT = 3333
+const PORT = 4444
 
 const app = express()
 
+const events = []
+
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-    res.send(`yup, it's running`)
-})
+app.use(
+    '/api',
+    graphqlHTTP({
+        schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
 
-app.listen(PORT, () => console.log(motd(PORT)))
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String
+            }
+
+            type RootQuery {
+                events: [Event!]!
+            }
+            type RootMutation {
+                createEvent(eventInput: EventInput): Event
+            }
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events: () => events,
+            createEvent: (args) => {
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: new Date().toISOString()
+                }
+                events.push(event)
+                return event
+            },
+        },
+        graphiql: true
+}))
+
+app.listen(PORT, () => {
+    console.clear()
+    console.log(motd(PORT))
+})
