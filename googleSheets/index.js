8cc09cb5bfac0eeb90ad81f84d2f84c6b4f90936
Miguel Diaz <diaz4674@gmail.com>
2020-12-07 17:55:38 +0000

    updated rows being read

--- googleSheets/index.js
+++ googleSheets/index.js
@@ -11,10 +11,10 @@ const TOKEN_PATH = "token.json";
 
 // Load client secrets from a local file.
 fs.readFile("keys.json", (err, content) => {
-	if (err) return console.log("Error loading client secret file:", err);
-	// Authorize a client with credentials, then call the Google Sheets API.
-	authorize(JSON.parse(content), listMajors);
-	// authorize(JSON.parse(content), getCodes);
+    if (err) return console.log("Error loading client secret file:", err);
+    // Authorize a client with credentials, then call the Google Sheets API.
+    authorize(JSON.parse(content), listMajors);
+    // authorize(JSON.parse(content), getCodes);
 });
 
 /**
@@ -24,20 +24,20 @@ fs.readFile("keys.json", (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-	const { client_secret, client_id, redirect_uris } = credentials.web;
-	console.log(credentials.web.client_id);
-	const oAuth2Client = new google.auth.OAuth2(
-		client_id,
-		client_secret,
-		redirect_uris[0]
-	);
+    const { client_secret, client_id, redirect_uris } = credentials.web;
+    console.log(credentials.web.client_id);
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+    );
 
-	// Check if we have previously stored a token.
-	fs.readFile(TOKEN_PATH, (err, token) => {
-		if (err) return getNewToken(oAuth2Client, callback);
-		oAuth2Client.setCredentials(JSON.parse(token));
-		callback(oAuth2Client);
-	});
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getNewToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+    });
 }
 
 /**
@@ -47,32 +47,32 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 function getNewToken(oAuth2Client, callback) {
-	const authUrl = oAuth2Client.generateAuthUrl({
-		access_type: "offline",
-		scope: SCOPES,
-	});
-	console.log("Authorize this app by visiting this url:", authUrl);
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-	rl.question("Enter the code from that page here: ", (code) => {
-		rl.close();
-		oAuth2Client.getToken(code, (err, token) => {
-			if (err)
-				return console.error(
-					"Error while trying to retrieve access token",
-					err
-				);
-			oAuth2Client.setCredentials(token);
-			// Store the token to disk for later program executions
-			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-				if (err) return console.error(err);
-				console.log("Token stored to", TOKEN_PATH);
-			});
-			callback(oAuth2Client);
-		});
-	});
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: SCOPES,
+    });
+    console.log("Authorize this app by visiting this url:", authUrl);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    rl.question("Enter the code from that page here: ", (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err)
+                return console.error(
+                    "Error while trying to retrieve access token",
+                    err
+                );
+            oAuth2Client.setCredentials(token);
+            // Store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err) return console.error(err);
+                console.log("Token stored to", TOKEN_PATH);
+            });
+            callback(oAuth2Client);
+        });
+    });
 }
 
 /**
@@ -82,24 +82,25 @@ function getNewToken(oAuth2Client, callback) {
  */
 // https://docs.google.com/spreadsheets/d/1x_PgDjeZ0UMk6wYGASQcnOFEMYXfRzWU22pnqNz-nP8/edit#gid=0
 function listMajors(auth) {
-	const sheets = google.sheets({ version: "v4", auth });
-	sheets.spreadsheets.values.get(
-		{
-			spreadsheetId: "1x_PgDjeZ0UMk6wYGASQcnOFEMYXfRzWU22pnqNz-nP8",
-			range: "A2:A7",
-		},
-		(err, res) => {
-			if (err) return console.log("The API returned an error: " + err);
-			const rows = res.data.values;
-			if (rows.length) {
-				console.log("Name, Major:");
-				// Print columns A and E, which correspond to indices 0 and 4.
-				rows.map((row) => {
-					console.log(`${row[0]}, ${row[4]}`);
-				});
-			} else {
-				console.log("No data found.");
-			}
-		}
-	);
+    const sheets = google.sheets({ version: "v4", auth });
+    sheets.spreadsheets.values.get(
+        {
+            spreadsheetId: "1x_PgDjeZ0UMk6wYGASQcnOFEMYXfRzWU22pnqNz-nP8",
+            // Gets each rows value, from start row cell, to the end row cell.
+            range: "A4:H100",
+        },
+        (err, res) => {
+            if (err) return console.log("The API returned an error: " + err);
+            const rows = res.data.values;
+            if (rows.length) {
+                console.log(rows);
+                // Print columns A and E, which correspond to indices 0 and 4.
+                rows.map((row) => {
+                    console.log(`${row[0]}, ${row[4]}`);
+                });
+            } else {
+                console.log("No data found.");
+            }
+        }
+    );
 }
