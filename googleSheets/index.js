9cbf62df48d595a8457455021d85d7536217a8d7
miguel diaz <diaz4674@gmail.com>
2020-12-07 20:16:58 -0800

    updated token

--- googleSheets/index.js
+++ googleSheets/index.js
@@ -12,10 +12,10 @@ const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 const TOKEN_PATH = "token.json";
 
 fs.readFile(normalizedPath, (err, content) => {
-    if (err) return console.log("Error loading client secret file:", err);
-    // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content), couponData);
-    // authorize(JSON.parse(content), getCodes);
+	if (err) return console.log("Error loading client secret file:", err);
+	// Authorize a client with credentials, then call the Google Sheets API.
+	authorize(JSON.parse(content), couponData);
+	// authorize(JSON.parse(content), getCodes);
 });
 
 /**
@@ -25,20 +25,20 @@ fs.readFile(normalizedPath, (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-    const { client_secret, client_id, redirect_uris } = credentials.web;
-    console.log(credentials.web.client_id);
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id,
-        client_secret,
-        redirect_uris[0]
-    );
+	const { client_secret, client_id, redirect_uris } = credentials.web;
+	console.log(credentials.web.client_id);
+	const oAuth2Client = new google.auth.OAuth2(
+		client_id,
+		client_secret,
+		redirect_uris[0]
+	);
 
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
-    });
+	// Check if we have previously stored a token.
+	fs.readFile(TOKEN_PATH, (err, token) => {
+		if (err) return getNewToken(oAuth2Client, callback);
+		oAuth2Client.setCredentials(JSON.parse(token));
+		callback(oAuth2Client);
+	});
 }
 
 /**
@@ -48,32 +48,32 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: "offline",
-        scope: SCOPES,
-    });
-    console.log("Authorize this app by visiting this url:", authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question("Enter the code from that page here: ", (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err)
-                return console.error(
-                    "Error while trying to retrieve access token",
-                    err
-                );
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log("Token stored to", TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
+	const authUrl = oAuth2Client.generateAuthUrl({
+		access_type: "offline",
+		scope: SCOPES,
+	});
+	console.log("Authorize this app by visiting this url:", authUrl);
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	rl.question("Enter the code from that page here: ", (code) => {
+		rl.close();
+		oAuth2Client.getToken(code, (err, token) => {
+			if (err)
+				return console.error(
+					"Error while trying to retrieve access token",
+					err
+				);
+			oAuth2Client.setCredentials(token);
+			// Store the token to disk for later program executions
+			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+				if (err) return console.error(err);
+				console.log("Token stored to", TOKEN_PATH);
+			});
+			callback(oAuth2Client);
+		});
+	});
 }
 
 /**
@@ -83,36 +83,35 @@ function getNewToken(oAuth2Client, callback) {
  */
 // https://docs.google.com/spreadsheets/d/1x_PgDjeZ0UMk6wYGASQcnOFEMYXfRzWU22pnqNz-nP8/edit#gid=0
 function couponData(auth) {
-    const sheets = google.sheets({ version: "v4", auth });
-    sheets.spreadsheets.values.get(
-        {
-            spreadsheetId: "1x_PgDjeZ0UMk6wYGASQcnOFEMYXfRzWU22pnqNz-nP8",
-            // Gets each rows value, from start row cell, to the end row cell.
-            range: "A4:H100",
-        },
-        (err, res) => {
-            if (err) return console.log("The API returned an error: " + err);
-            const rows = res.data.values;
-            if (rows.length) {
-                let coupons = []
-                // Print columns A and E, which correspond to indices 0 and 4.
-                rows.map((row) => {
-                    // console.log("title:", row[0], "link:", row[2])
-                    coupons.push({
-                        "title": row[0],
-                        "code": row[1],
-                        "link": row[2],
-                        "price": row[3],
-                        "discount": row[4],
-                        "category": row[6],
-                        "image": row[7]
-                    })
-                    console.log(coupons)
-                });
-            } else {
-                console.log("No data found.");
-            }
-        }
-    );
+	const sheets = google.sheets({ version: "v4", auth });
+	sheets.spreadsheets.values.get(
+		{
+			spreadsheetId: "1x_PgDjeZ0UMk6wYGASQcnOFEMYXfRzWU22pnqNz-nP8",
+			// Gets each rows value, from start row cell, to the end row cell.
+			range: "A4:H100",
+		},
+		(err, res) => {
+			if (err) return console.log("The API returned an error: " + err);
+			const rows = res.data.values;
+			if (rows.length) {
+				let coupons = [];
+				// Print columns A and E, which correspond to indices 0 and 4.
+				rows.map((row) => {
+					// console.log("title:", row[0], "link:", row[2])
+					coupons.push({
+						title: row[0],
+						code: row[1],
+						link: row[2],
+						price: row[3],
+						discount: row[4],
+						category: row[6],
+						image: row[7],
+					});
+					console.log(coupons);
+				});
+			} else {
+				console.log("No data found.");
+			}
+		}
+	);
 }
-
