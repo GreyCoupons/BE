d2fe77be6f1e3c4d4b2ce5bab1c713ecfb26f0ce
miguel diaz <diaz4674@gmail.com>
2020-12-12 01:25:18 -0800

    production migrate file

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
 		client: "pg",
 		connection: process.env.DATABASE_URL,
 		migrations: {
-			directory: __dirname + "/db/migrations",
+			tableName: "knex_migrations",
 		},
 	},
 };
