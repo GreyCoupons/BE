881d2b2281820bc1bab485c2bf876c753f24274d
miguel diaz <diaz4674@gmail.com>
2020-12-12 02:01:17 -0800

    added knex production

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 // DEPENDENCIES
-const { add_flags } = require("./middleware")
-const express = require("express")
-const cors = require("cors")
-const helmet = require("helmet")
-const app = express()
+const { add_flags } = require("./middleware");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const app = express();
+const knex = require("knex");
+const knexConfig = require("./knexfile");
+const environment = process.env.DB_ENV || "development";
 
 // IMPORTS
-require("./motd/motd")
-const PORT = process.env.PORT || 3333
+require("./motd/motd");
+const PORT = process.env.PORT || 3333;
 
 // GLOBAL MIDDLEWARE
-app.use(helmet())
-app.use(cors())
-app.use(express.json())
-app.use(add_flags)
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(add_flags);
 
 // API
-const routes = require("./routes")
-routes(app)
+const routes = require("./routes");
+routes(app);
 
 // SERVER MESSAGE
 app.listen(PORT, () => {
-	console.clear()
-	console.log(motd(PORT))
-})
+	console.clear();
+	console.log(motd(PORT));
+});
+module.exports = knex(knexConfig[environment]);
