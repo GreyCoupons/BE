5a00d642d64424ff632e841c8fdffe23405b762c
miguel diaz <diaz4674@gmail.com>
2020-12-12 01:52:37 -0800

    updated knex db

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,11 @@ require("dotenv").config();
 module.exports = {
 	development: {
 		client: "pg",
-		connection: process.env.DATABASE_URL,
+		connection: process.env.DATABASE_URL || {
+			database: "test",
+			user: "postgres",
+			password: "idontknow",
+		},
 		pool: {
 			min: 2,
 			max: 10,
@@ -12,6 +16,7 @@ module.exports = {
 		migrations: {
 			tableName: "knex_migrations",
 		},
+		useNullAsDefault: true,
 	},
 	production: {
 		production: {
