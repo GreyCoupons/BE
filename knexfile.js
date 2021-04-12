a4747b7bd9e1880c1d3904cc20f93823e1ebd8ff
miguel diaz <diaz4674@gmail.com>
2021-04-11 21:52:31 -0700

    ssl

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
 	production: {
 		client: process.env.DB_CLIENT || "pg",
 		connection: process.env.DATABASE_URL,
-		PGSSLMODE=require,
+		ssl=true,
 		migrations: {
 			tableName: "knex_migrations",
 		},
